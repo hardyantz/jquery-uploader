cf8209a308e9e0e4b03bf3dd445f099914017404
hardyantz <hardyantz@yahoo.com>
2016-10-27 14:59:22 +0700

    Update jquery.upload-preview.js

--- src/js/jquery.upload-preview.js
+++ src/js/jquery.upload-preview.js
@@ -1,132 +1,224 @@
-(function ($) {
-  var jupload_id_div_image = 'image-upload'
-  var jupload_div_image = '#' + jupload_id_div_image;
+(function($) {
+    var jupload_id_div_image = 'image-upload'
+    var jupload_div_image = '#' + jupload_id_div_image;
+    var text_clickable = '<div class="noimg-message">Drop files here or click to upload</div>';
 
-  $.extend({
-    uploadPreview : function (options) {
+    // is IE
+    var isIE = /*@cc_on!@*/false || !!document.documentMode;
+    // Edge 20+
+    var isEdge = !isIE && !!window.StyleMedia;
 
-        // Options + Defaults
-        var settings = $.extend({
-            input_box: ".upload-preview",
-            input_field: ".upload",
-            preview_box: ".image-preview",
-            files_name: "uploader",
-            label_field: ".image-label",
-            label_default: "Choose File",
-            label_selected: "Change File",
-            no_label: false
-        }, options);
+    $.extend({
+        uploadPreview: function(options) {
 
-        $('<input type="text" id="count-file-input" value="0"/>').insertAfter(settings.input_box);
+            // Options + Defaults
+            var settings = $.extend({
+                input_box: ".upload-preview",
+                input_field: ".upload",
+                preview_box: ".image-preview",
+                files_name: "mmdc_scbundle_morisproduct[uploads]",
+                label_field: ".image-label",
+                label_default: "Choose File",
+                label_selected: "Change File",
+                no_label: false
+            }, options);
 
-        $(settings.input_box).on(
-            'dragover',
-            function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-          }
-        )
-        $(settings.input_box).on(
-            'dragenter',
-            function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-            }
-        )
+            $('<input type="hidden" id="count-file-input" value="0"/>').insertAfter(settings.input_box);
+            $(settings.input_box).html(text_clickable);
 
-        $(settings.input_box).on(
-            'drop',
-            function(e){
+            $(settings.input_box + ', .noimg-message').on(
+                'dragover',
+                function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+            )
+            $(settings.input_box + ', .noimg-message').on(
+                'dragenter',
+                function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
 
-                $('<input type="file" id="'+jupload_id_div_image+'-' +$("#count-file-input").val()+ '" name="'+settings.files_name+'[]" multiple/>').insertAfter(settings.input_box);
-                if(e.originalEvent.dataTransfer){
-                    if(e.originalEvent.dataTransfer.files.length) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        var imageLoader = document.getElementById(jupload_id_div_image +'-'+$("#count-file-input").val());
+                }
+            )
 
-                        /*UPLOAD FILES HERE*/
-                        var dt = e.originalEvent.dataTransfer;
-                        var files = dt.files;
-                        console.log(files);
-                        //this code line fires your 'handleImage' function (imageLoader change event)
-                        imageLoader.files = files;
-                        iterateFiles(e.originalEvent.dataTransfer.files, settings);
-                        $("#count-file-input").val($("#count-file-input").val() + 1);
+            $(settings.input_box + ', .noimg-message').on(
+                'drop',
+                function(e) {
+                    $('.noimg-message').remove();
+                    $('<input  type="file" style="display:none;"  class="image-upload-class" id="' + jupload_id_div_image + '-' + $("#count-file-input").val() + '" name="' + settings.files_name + '[]" multiple/>').insertAfter(settings.input_box);
+
+                    if (e.originalEvent.dataTransfer) {
+                        if (e.originalEvent.dataTransfer.files.length) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            var imageLoader = document.getElementById(jupload_id_div_image + '-' + $("#count-file-input").val());
+                            var dt = e.originalEvent.dataTransfer;
+                            var files = dt.files;
+
+                            imageLoader.files = files;
+                            var idInput = jupload_id_div_image + '-' + $("#count-file-input").val();
+                            iterateFiles(e.originalEvent.dataTransfer.files, settings, idInput);
+                        }
                     }
                 }
+            );
+
+            $(settings.input_box + ', .noimg-message').on('click', function(e) {
+                e.preventDefault();
+                if (e.target !== this) {
+                    return;
+                }
+
+                $('<input type="file" style="" class="image-upload-class" id="' + jupload_id_div_image + '-' + $("#count-file-input").val() + '" name="' + settings.files_name + '[]" multiple/>').insertAfter(settings.input_box);
+                
+                if (isIE) {
+                    $(jupload_div_image + '-' + $("#count-file-input").val()).on('change', handleIEImage);
+                }
+                $(jupload_div_image + '-' + $("#count-file-input").val())[0].click();
+                var imageLoader = document.getElementById(jupload_id_div_image + '-' + $("#count-file-input").val());
+                
+                if (isIE) {
+                    //imageLoader.addEventListener('load', handleImage, false);
+                } else {
+                    imageLoader.addEventListener('change', handleIEImage, false);
+                }
+            });
+
+            if ((window.File && window.FileList && window.FileReader) === false) {
+                alert("Browser not compatible for image uploader.");
+                return false;
             }
-        );
 
-        //$(jupload_div_image).hide();
+            function handleIEImage(e) {
+                var files = this.files;
+                if (files.length > 0) {
+                    var idInput = jupload_id_div_image + '-' + $("#count-file-input").val();
+                    iterateFiles(files, settings, idInput);
 
-        $(settings.input_box).on('click', function(e){
-            e.preventDefault();
-            $('<input type="file" class="image-upload-class" id="'+jupload_id_div_image+'-' + $("#count-file-input").val() +'" name="'+settings.files_name+'[]" multiple/>').insertAfter(settings.input_box);
-            $(jupload_div_image+'-'+$("#count-file-input").val())[0].click();
-            var imageLoader = document.getElementById(jupload_id_div_image +'-'+$("#count-file-input").val());
-            imageLoader.addEventListener('change', handleImage, false);
-        })
+                    $('.noimg-message').remove();
+                    var imageLoader = document.getElementById(idInput);
+                    imageLoader.files = files;
+                } else {
+                    var text_clickable = '<div class="noimg-message">Drop files here or click to upload</div>';
 
-        // Check if FileReader is available
-        if ((window.File && window.FileList && window.FileReader) === false) {
-            alert("Browser not compatible for image uploader.");
+                    $(settings.input_box).html(text_clickable);
+                    return false;
+                }
+            }
+        }
+    });
+
+    function handleImage(e) {
+        var files = this.files;
+        if (files.length > 0) {
+            var idInput = jupload_id_div_image + '-' + $("#count-file-input").val();
+            iterateFiles(files, settings, idInput);
+
+            $('.noimg-message').remove();
+            var imageLoader = document.getElementById(idInput);
+            imageLoader.files = files;
+        } else {
+            var text_clickable = '<div class="noimg-message">Drop files here or click to upload</div>';
+
+            $(settings.input_box).html(text_clickable);
             return false;
         }
-
-        function handleImage(e) {
-            console.log(this.files);
-            var files = this.files;
-            if (files.length > 0) {
-                iterateFiles(files, settings);
-                $('.message').remove();
-                var imageLoader = document.getElementById(jupload_id_div_image +'-'+$("#count-file-input").val());
-                imageLoader.files = files;
-                $("#count-file-input").val($("#count-file-input").val() + 1);
-            } else {
-                var text_clickable ='<div class="message needsclick">Drop files here or click to upload</div>';
-                // Clear background
-                $(settings.input_box).html(text_clickable);
-            }
-        };
-
-        $( settings.input_box ).sortable();
-        $( settings.input_box ).disableSelection();
     }
-  });
 
-  function iterateFiles(files, settings) {
-    var count_preview = $(".preview-item").length;
+    function iterateFiles(files, settings, idInput) {
 
-    $.each( files, function( i, file ) {
-        var index = i + count_preview;
-
-        var reader = new FileReader();
-
-        // Load file
-        reader.addEventListener("load",function(event) {
-            var loadedFile = event.target;
-
-            // Check format
-            if (file.type.match('image')) {
-                // Image
-                renderPreview(loadedFile, index, settings);
-            } else {
-                alert("This file type is not supported yet.");
-            }
-        });
-
-        if (settings.no_label == false) {
-        // Change label
-        $(settings.label_field).html(settings.label_selected);
+        if ($(".preview-item").length > 0) {
+            $('.noimg-message').remove();
         }
 
-        // Read the file
-        reader.readAsDataURL(file);
-    });
-  }
-  function renderPreview(loadedFile, i, settings) {
-    $( settings.input_box ).append( "<div class='preview-item' id='preview-" + i + "''></div>" );
-    $("#preview-" + i).html('<img src="'+loadedFile.result+'">');
-  }
+        var count_preview = $("#count-file-input").val();
+
+        for (var l = 0; l < files.length; l++) {
+            if (files[l].type.match('image') == null) {
+                $('#' + idInput).remove();
+                alert(files[l].name + ", This file type is not supported yet.");
+                return;
+            }
+        }
+
+        $.each(files, function(i, file) {
+            var index = i + count_preview;
+
+            var reader = new FileReader();
+
+            reader.addEventListener("load", function(event) {
+                renderPreview(event, index, settings, file.name, idInput);
+
+            });
+
+            if (settings.no_label == false) {
+                $(settings.label_field).html(settings.label_selected);
+            }
+
+            reader.readAsDataURL(file);
+        });
+
+        if (typeof $(settings.input_box).sortable("instance") != 'undefined') {
+            $(settings.input_box).sortable("destroy");
+        }
+        $(settings.input_box).sortable({
+            scroll: true,
+            items: "> .preview-item",
+            stop: function(event, ui) {
+                setPrimary();
+            }
+        });
+        $(settings.input_box).disableSelection();
+    }
+
+    function renderPreview(loadedFile, i, settings, filename, idInput) {
+
+        if (loadedFile.target.result !== '') {
+            image = new Image();
+            image.src = loadedFile.target.result;
+            image.onload = function() {
+                var w = this.width,
+                    h = this.height;
+
+                if (((w < 500) || (w > 2000)) || ((h < 500) || (h > 2000))) {
+                    alert(filename + ', invalid image size');
+                } else {
+
+                    $(settings.input_box).append("<div class='preview-item " + idInput + "' input-file='" + idInput + "' id='preview-" + i + "'></div>");
+                    var sequence_input = '<input type="hidden" name="sequence[]" value="' + filename + '">';
+                    var remove_button = '<div class="img-overlay"><button class="btn btn-md btn-danger" data-id="' + i + '" id="remove-' + i + '">Remove</button></div>';
+                    $('#preview-' + i).html('<div class="img-wrapper"><img src="' + loadedFile.target.result + '" class="img-responsive img-list"> ' + remove_button + ' </div>' + sequence_input);
+                    var remove_id = document.getElementById('remove-' + i);
+                    remove_id.addEventListener('click', removeBox(settings.input_box, i, idInput), false);
+
+                    setPrimary();
+                }
+            }
+        }
+
+        $("#count-file-input").val(parseInt($("#count-file-input").val()) + 1);
+    }
+
+    function removeBox(input_box, id, idInput) {
+        return function() {
+            $("#preview-" + id).remove();
+            if ($(".preview-item").length <= 0) {
+                $(input_box).html(text_clickable);
+            }
+
+            if ($("." + idInput).length <= 0) {
+                $("#" + idInput).remove();
+            }
+
+            setPrimary();
+        }
+    }
+
+    function setPrimary() {
+        var primary_img = '<i class="fa fa-check-circle-o fa-4x text-success" id="primary-img" aria-hidden="true" style="position: absolute;margin-top: -20px;margin-left: -20px;"></i>';
+        $("#primary-img").remove();
+        $(".preview-item").first().append(primary_img);
+
+    }
 })(jQuery);
